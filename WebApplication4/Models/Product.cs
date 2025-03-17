ebbd8adb4913ac8563ea1c36065cb7e5082ea817
pallavi <pallavisavant02@gmail.com>
2025-03-16 20:22:43 -0500

    Final code changes

---
+++ WebApplication4/Models/Product.cs
@@ -0,0 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace WebApplication4.API.Models
+{
+    public class Product{
+        public int Id {get; set; }
+        [Required]
+        public string Sku { get; set; } = string.Empty;
+        [Required]
+        public string Name {get; set; } = string.Empty;
+        [Required]
+        public string Description {get; set; } = string.Empty;
+
+        public decimal Price {get; set; }
+        public bool IsAvailable {get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+        [JsonIgnore]
+        public virtual Category? Category { get; set; }
+
+
+    }
+}
